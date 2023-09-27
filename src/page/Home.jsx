bfe7ef61abff21acec0f95ed8ce0c6da527879c7
Karen-Hoil <karenhoil2004@gmail.com>
2023-09-27 10:40:14 -0500

    Se agrego los integrantes -K

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,8 +7,14 @@ function Home() {
     <div className='dashboard1'>
     <Header/>
     <h1 className='Bienvenida'>
-        Bienvenidos
+        Portafolio donde se gusardaran nuestras actividades
     </h1>
+    <div className='Integrantes'>
+    <p>Integrantes:</p>
+    <h4>Ana Karen Hoil Hoil</h4>
+    <h4>Gustavo Gutierrez Canul</h4>
+    <h4>Fernando Gomez Toleto</h4>
+    </div>
     </div>
     </>
   )
